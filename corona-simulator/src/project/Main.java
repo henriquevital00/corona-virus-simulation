7e6ac3abbd120da41b861d62d7cf09d5a1269391
henriquevital00 <henriquevital1000@hotmail.com>
2020-12-12 09:51:34 -0300

    Added verification to stop simulation

--- corona-simulator/src/project/Main.java
+++ corona-simulator/src/project/Main.java
@@ -7,6 +7,7 @@ package project;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Scanner;
 
 /**
  *
@@ -19,12 +20,19 @@ public class Main {
      */
     public static void main(String[] args) {
         int time=0;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter with the number of health people: ");
+        int healthPeople = in.nextInt();
+        System.out.print("\nEnter with the number of sick people: ");
+        int sickPeople = in.nextInt(); 
+        System.out.println("");
+        
         Virus virus = new Virus(0);
-
         Mundo mundo = new Mundo();
         mundo.createHospital();
         mundo.createMap();
-        mundo.setPeople(100, 1);
+        mundo.setPeople(healthPeople, sickPeople);
         while(true) {
             mundo.desenhaMundo(time);
             mundo.changePosition();
@@ -32,8 +40,20 @@ public class Main {
             mundo.updateMorte();
             mundo.checkIfDie();
             time++;
+            if (mundo.getHealthPeople() == 0){
+                System.out.println("Finishing Simulation...");
+                System.out.println("All health people was infected");
+            }
+            else if (mundo.getSickPeople() == 0){
+                System.out.println("Finishing Simulation...");
+                System.out.println("All health sick was heal");
+            }
+            else if (mundo.getHealthPeople() + mundo.getSickPeople() == 0){
+                System.out.println("Finishing Simulation...");
+                System.out.println("All people died");
+            }
             try {
-                Thread.currentThread().sleep(1000); // 1 segundo
+                Thread.currentThread().sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
